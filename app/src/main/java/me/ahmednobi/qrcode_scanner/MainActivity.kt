1e52c6896fe01500eed27ad9f5f41b83e10db960
AhmedNobi <a.nobi95@eng.aswu.edu>
2020-03-11 18:45:00 +0200

    Intial

---
+++ app/src/main/java/me/ahmednobi/qrcode_scanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.ahmednobi.qrcode_scanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
